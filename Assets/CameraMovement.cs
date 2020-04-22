6e0c9fb2f39570062585ba66cf27eb852e96281f
scuderiricky <63512739+scuderiricky@users.noreply.github.com>
2020-04-22 15:39:54 +0200

    PLayer,Video mit Audio hinzugefügt

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float M_sensivity = 100f;
+    public Transform playerBody;
+    private float _localRotX = 0f;
+    private float _localRotY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _localRotX += Input.GetAxis("Mouse X") * M_sensivity * Time.deltaTime;
+        _localRotX = _localRotX % 360;
+        _localRotY += Input.GetAxis("Mouse Y") * M_sensivity * Time.deltaTime;
+        _localRotY = Mathf.Clamp(_localRotY,-90f,90f);
+
+        var angleRightLeft = Quaternion.AngleAxis(_localRotX,Vector3.up);
+        var angleUpDown = Quaternion.AngleAxis(_localRotY, Vector3.left);
+        playerBody.rotation = angleRightLeft;
+        transform.localRotation = angleUpDown;
+        
+    }
+}
